da2f9aab63d5ad18f1f132b64d077cb81d9fe5bd
huang <h_peifen@163.com>
2020-04-27 17:33:22 +0800

    配置

---
+++ src/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <p>{{ greeting }} world!</p>
+    </div>
+</template>
+<script>
+module.exports= {
+// export default {
+    components: {},
+    props: {},
+    data() {
+        return {
+            greeting: 'Hello'
+        };
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    methods: {},
+// };
+}
+</script>
+<style lang="less">
+.container{
+    p{
+        color: red
+    }
+}
+</style>
--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,16 @@
 // 公用配置
 const path = require('path');
 const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin') // 添加vue-loader
+
 module.exports = {
   entry: {
     app: './src/vue.js',
   },
   plugins: [
-
+    new VueLoaderPlugin() // 引入vue-loader
   ],
+  mode: 'production',
   output: {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].bundle.js', // 决定非入口 chunk 的名称
@@ -16,11 +19,67 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // 排除node_modules内的转译
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        )
+      },
+      {
           test: /\.css$/, // 根据正则表达式，来确定应该查找哪些文件
           use: [ // 将其提供给指定的 loader (style-loader 和 css-loader)
               'style-loader',
-              'css-loader'
+              'css-loader',
+              'vue-style-loader'
           ]
+      },
+      {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                // 你也可以从一个文件读取，例如 `variables.scss`
+                // 如果 sass-loader 版本 < 8，这里使用 `data` 字段
+                prependData: `$color: red;`
+              }
+            }
+          ]
+      },
+      {
+          test: /\.less$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'less-loader'
+          ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              // sass-loader version >= 8
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
       },
       {
           test: /\.(png|svg|jpg|gif)$/,
