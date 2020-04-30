85cb5d5cdc76b9b1f84c6bcef00d88b334bcb2db
huang <h_peifen@163.com>
2020-04-30 09:57:09 +0800

    研究热替换

--- src/webpack/index.js
+++ src/webpack/index.js
@@ -1,7 +1,7 @@
-import printMe from './print.js';
-import './styles.css';
-import _ from 'lodash'
-import { cube } from './math.js';
+// import printMe from './print.js';
+// import './styles.css';
+// import _ from 'lodash'
+// import { cube } from './math.js';
 function getComponent() {
     var element = document.createElement('div');
     var button = document.createElement('button');
@@ -9,7 +9,7 @@ function getComponent() {
     
     button.innerHTML = 'Click me and look at the console!';
 
-    element.innerHTML = _.join(['Hello', 'webpack'], cube());
+    // element.innerHTML = _.join(['Hello', 'webpack'], cube());
     element.appendChild(br);
     element.appendChild(button);
     // button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
@@ -25,11 +25,11 @@ function getComponent() {
 var domShow = getComponent(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 document.body.appendChild(domShow);
 
-if (module.hot) {
-    module.hot.accept('./print.js', function () {
-        console.log('热模块的内容');
-        document.body.removeChild(domShow);
-        domShow = getComponent(); // 重新渲染页面后，getComponent 更新 click 事件处理
-        document.body.appendChild(domShow);
-    })
-}
+// if (module.hot) {
+//     module.hot.accept('./print.js', function () {
+//         console.log('热模块的内容');
+//         document.body.removeChild(domShow);
+//         domShow = getComponent(); // 重新渲染页面后，getComponent 更新 click 事件处理
+//         document.body.appendChild(domShow);
+//     })
+// }
